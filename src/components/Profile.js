b6d366d50a491fd39caf025d7b9d6f5f055ac22f
NGO VIET NHAT MINH <minh.nvn@cubevn.local>
2023-06-14 16:27:50 +0700

    Feature: added AAT-29

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,31 @@
-//Link with UserHandler.js in ./server
+import React from "react";
+import { useState, useEffect } from "react";
+import profile from "../img/Profile.png";
+import PowerButton from "../img/PowerButton.png";
 
-function Profile () {
-    return (
-        1
-    )
+function Profile() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <div className="col-1 dropdown">
+      <button className="dropbtn" onClick={handleOpen}>
+        <img className="profile" src={profile} alt="Profile" />
+      </button>
+      {open ? (
+        <div className="dropdown-content">
+          <a href="#">My Profile</a>
+          <a href="#">Reset Password</a>
+          <a href="#">
+            Sign Out{" "}
+            <img className="powerbutton" src={PowerButton} alt="PowerButton" />{" "}
+          </a>
+        </div>
+      ) : null}
+    </div>
+  );
 }
 
-export {Profile}
+export default Profile;
--- src/mainClient/mainpage/MainPage.js
+++ src/mainClient/mainpage/MainPage.js
@@ -1,45 +1,41 @@
-import {Login, Register, ResetPwd} from '../../components/MainInput'
-import {LoginValid, PwdValid, RegValid} from '../../components/Validation'
-import Multi_Lang from '../../components/Multi_Lang'
-import '../../../node_modules/bootstrap/dist/css/bootstrap.css'
-import './MainPage.css'
-import { React} from 'react';
+import { Login, Register, ResetPwd } from "../../components/MainInput";
+import { LoginValid, PwdValid, RegValid } from "../../components/Validation";
+import Multi_Lang from "../../components/Multi_Lang";
+import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
+import "./MainPage.css";
+import { React } from "react";
+import Profile from "components/Profile";
 
 const MainPage = () => {
   return (
     <>
-    <div className="header">
+      <div className="header">
         <div className="col-2">
-        <link href="//db.onlinewebfonts.com/c/11cd399ec653d4e156ce034b85c19cb7?family=VLNL+Decks" rel="stylesheet" type="text/css"/>
-          <p className="logo">
-            AISIN
-          </p><br></br>
-          <p className="col logotext">
-            We Touch The Future
-          </p>
-          
+          <link
+            href="//db.onlinewebfonts.com/c/11cd399ec653d4e156ce034b85c19cb7?family=VLNL+Decks"
+            rel="stylesheet"
+            type="text/css"
+          />
+          <p className="logo">AISIN</p>
+          <br></br>
+          <p className="col logotext">We Touch The Future</p>
         </div>
         <div className="label col">
           <p className="label">Welcom to AISIN Parts Support</p>
         </div>
-        <Multi_Lang/>
-    </div>
-    <div className="body">
-        <Login/>
+        <Profile />
+      </div>
+      <div className="body">
+        <Login />
         <div className="col-sm-12 col-md content">
-          <p className="logo">
-            AISIN
-          </p><br></br>
-          <p className="contenttext">
-            AIMING FOR A BETTER FUTURE
-          </p>
-          
+          <p className="logo">AISIN</p>
+          <br></br>
+          <p className="contenttext">AIMING FOR A BETTER FUTURE</p>
         </div>
         <div className="col body-img"></div>
       </div>
     </>
   );
-
-}
+};
 
 export default MainPage;
