9dcb5d342f31fa7823d1d8cb1cacb526a68fd521
khoa.tld <khoa.tld@vn-cubesystem.com>
2023-06-14 13:48:59 +0700

    Feature: Added AAT-28

--- src/components/LanguageOption.js
+++ src/components/LanguageOption.js
@@ -3,10 +3,8 @@ import React, { useState } from 'react'
 import './comp.styles/LangOption.css'
 
 const languages = [
-    { value: '', text: "Default EN" },
-    { value: 'en', text: "English" },
-    { value: 'vi', text: "Vietnam" },
-    { value: 'ja', text: "Japanese" },
+  { value: 'vi', text: "Vietnam" },
+  { value: 'en', text: "English" },
   ]
   
   function AppRender () {
@@ -32,39 +30,3 @@ const languages = [
   }
 
   export {AppRender}
-  
-import '../i18n'
-import React, { useState } from 'react'
-import './comp.styles/LangOption'
-
-const languages = [
-    { value: '', text: "Default EN" },
-    { value: 'en', text: "English" },
-    { value: 'vi', text: "Vietnam" },
-    { value: 'ja', text: "Japanese" },
-  ]
-  
-  function AppRender () {
-    //Multilang
-    var [lang, setLang] = useState('');
-    const handleChange = e => {
-      setLang(e.target.value);
-      let loc = "http://localhost:3000/";
-      window.location.replace(loc + "?lng=" + e.target.value);
-    }
-    
-      //Export
-      return (
-        <>
-          <select value={lang} onChange={handleChange}>
-            {languages.map(item => {
-                return (<option key={item.value}
-                    value={item.value}>{item.text}</option>);
-            })}
-        </select>
-        </>
-      );
-  }
-
-  export {AppRender}
-  
