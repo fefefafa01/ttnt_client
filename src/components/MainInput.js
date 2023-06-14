36d192c3029c3170e960e022ac845a3689b6f5c1
NGO VIET NHAT MINH <minh.nvn@cubevn.local>
2023-06-14 08:43:33 +0700

    feature:added AAT-31

--- src/components/MainInput.js
+++ src/components/MainInput.js
@@ -1,56 +1,137 @@
 import { EyeInvisibleOutlined, EyeOutlined } from '@ant-design/icons';
 import { React, useState } from 'react';
+import { LoginValid, PwdValid } from './Validation';
+import { Link, Routes, Route } from 'react-router-dom';
 
-function Login () {
-    const[password, setPassword] = useState("");
-    const[visible, setVisible] = useState(false);
-    return (
-    <div className="col-xs-9 col-md-7 col-lg-3 wrapper">
-        <div className="form-box login">
+function Login(){
+    const [popupStyle, showPopup] = useState("hide")
+    const popup = () => {
+        showPopup("login-popup")
+        setTimeout(() => showPopup("hide"), 3000)
+    }
+    
+  return (
+    <div className="wrapper">
+        <div className="form-box">
             <h2>Login</h2>
-            <form action="#">
+            <form action="">
                 <div className="input-box">
-                    <input type="email" placeholder='Email Address' required />
+                    <span className="icon"></span>
+                    <input type="email" placeholder='Email Address'></input>
                 </div>
                 <div className="input-box">
-                    <input  value={password}
-                            type={visible ? "text" : "password"} 
-                            placeholder='Password' 
-                            onChange={(e) => setPassword(e.target.value)}
-                            required />
-                    <div className="p-2" onClick={() => setVisible(!visible)}>
-                        {visible ? <EyeOutlined/> : <EyeInvisibleOutlined/>}
-                    </div>
+                    <span className="icon"></span>
+                    <input type="password" placeholder='Password'></input>
                 </div>
-            </form>
-        </div>
-        <div className="remember-forgot">
-            <label htmlFor="">
-                <input type={'checkbox'} />
-                Remember 
-            </label>
-            <a href="#">Forgot Password?</a>
-        </div>  
-        <button className="btn" type='submit'>
-            User Login
-        </button>
-        <div className="login-register">
-            <p><a href="#" className="register-link">Create Account</a></p>
+                <div className="remember-forgot">
+                    <label htmlFor="">
+                        <input type={'checkbox'} />
+                            Remember 
+                    </label>
+                    <Routes>
+                        <Route path='/resetPwd' element={<ResetPwd/>}></Route>
+                    </Routes>
+                    <label htmlFor="">
+                        <Link to="/resetPwd" className="register-link">Forgot Password?</Link>
+                    </label>    
+                </div>  
+                <button className="btn" type='submit' onClick={popup}>
+                    Login
+                </button>
+                <div className={popupStyle}>
+                    <h3>HIIIII</h3>
+                </div>
+                <Routes>
+                        <Route path='/register' element={<Register/>}></Route>
+                    </Routes>
+                <div className="login-register">
+                    <p><Link to="/register"  className="register-link">Create Account</Link></p>
+                </div>
+                </form>
         </div>
     </div>
-    )
-}
+  );
+  };
 
 function Register () {
     return (
-        1
-    )
-}
+        <div className="wrapper">
+            <div className="form-box">
+                <h2>Sign Up</h2>
+                <form action="#">
+                    <div className="input-box">
+                        <span className="icon"></span>
+                        <input type="email" placeholder='Email Address' required />
+                    </div>
+                    <div className="input-box">
+                        <span className="icon"></span>
+                        <input type="password" placeholder='Password' required />
+                    </div>
+                    <div className="remember-forgot">
+                        <label htmlFor="">
+                            <input type={'checkbox'} />
+                                Remember 
+                        </label>
+                        Forgot Password?
+                    </div>  
+                    <button className="btn" type='submit'>
+                        Login
+                    </button>
+                    <div className="login-register">
+                        <p>abc</p>
+                        
+                    </div>
+                    </form>
+            </div>
+        </div>
+      );
+    };
 
-function ResetPwd () {
-    return (
-        1
-    )
-}
+function ResetPwd(){
+    const [values, setValues] = useState({
+        email: '',
+        password:''
+    })
+    const [errors, setErrors] = useState({})
+    const handleInput = (event) => {
+        setValues(prev => ({...prev, [event.target.className]: [event.target.value]}));
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setErrors(PwdValid(values));
+    }
+
+  return (
+    <div className="wrapper">
+        <div className="form-box">
+            <h2>Reset Password</h2>
+            <form action="" onSubmit={handleSubmit}>
+                <div className="input-box">
+                    <span className="icon"></span>
+                    <input type="email" placeholder='Email Address' 
+                    onChange={handleInput} required />
+                    {errors.email && <span className = 'text-danger'> {errors.email}</span>}
+                </div>
+                <div className="input-box">
+                    <span className="icon"></span>
+                    <input type="password" placeholder='Password' 
+                    onChange={handleInput} required />
+                    {errors.email && <span className = 'text-danger'> {errors.email}</span>}
+                </div> 
+                <div className="input-box">
+                    <span className="icon"></span>
+                    <input type="confirm password" placeholder='Confirm New Password' 
+                    onChange={handleInput} required />
+                    {errors.email && <span className = 'text-danger'> {errors.email}</span>}
+                </div>
+                    <button className="btn" type='submit'>
+                        Reset Password
+                    </button>
+                <p></p>
+            </form>
+            </div>
+        </div>
+  )
+  };
 
 export {Login, Register, ResetPwd}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom'
+import { render } from '@testing-library/react';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const reportWebVitals = onPerfEntry => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
+  }
+};
+
+render(
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
+reportWebVitals()
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
