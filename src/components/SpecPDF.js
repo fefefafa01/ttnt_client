87b6b06b5606591d1ae717823f318cca962c5e6e
LeTranKhanh <khanh.lt@vn-cubesystem.com>
2023-06-30 15:41:12 +0700

    Feat: Uploading Actual Data from Database

--- src/components/SpecPDF.js
+++ src/components/SpecPDF.js
@@ -2,13 +2,41 @@ import './comp.styles/SpecPDF.css'
 import { Changer } from "./LanguageChange"
 import spec from '../img/car.png'
 import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
+import { useState } from 'react';
 
-function Specpdf() {
+function Specpdf({carid}) {
     //Variables
-    var numOfPremium = 0, numOfSubPre = 0;
+    carid='1' //Test ID
+    const [firstOpenModel, setFirstOpenModel] = useState(true);
+    const [firstOpenPreP, setFirstOpenPreP] = useState(false);
+    const [firstOpenSPreP, setFirstOpenSPreP] = useState(false);
+    const [firstOpenComp, setFirstOpenComp] = useState(false);
+
+        //Variables for Car Model
+    var [maker, setMaker] = useState("")
+    var [model, setModel] = useState("")
+    var [vcode, setVcode] = useState("")
+    var [start, setStart] = useState("")
+    var [end, setEnd] = useState("")
+    var [dpos, setDpos] = useState("")
+    var [ecode, setEcode] = useState("")
+    var [displace, setDisplace] = useState("")
+    var [ptype, setPtype] = useState("")
+    var [ftype, setFtype] = useState("")
+    var [transc, setTransc] = useState("")
+    var [spd, setSpd] = useState("")
+    var [trans, setTrans] = useState("")
+    var [dt, setDt] = useState("")
+
+        //Array for Parts Table and Manufacturer
+    var [premiumData, setPremiumData] = useState([])
+    var [spremiumData, setSPremiumData] = useState([])
+    var [competitor, setCompetitor] = useState([])
+
     //Backend Call
-    const handleOpen = (e) => { //Cant Access now to Const as to control
-        fetch("http://localhost:5000/exp/details", {
+        //Querying Model Name
+    if (firstOpenModel) { 
+        fetch("http://localhost:5000/exp/model", {
             method: "POST",
             credentials: "include",
             headers: {
@@ -16,7 +44,7 @@ function Specpdf() {
                 "Acess-Control-Allow-Origin": "*",
                 "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, PATCH, OPTIONS"
             },
-            body: JSON.stringify(/* Car Model which parts are needed to be showed */)
+            body: JSON.stringify(carid)
         })
         .catch(err => {
             return;
@@ -29,117 +57,118 @@ function Specpdf() {
         })
         .then(data => {
             if (!data) return;
-            //data.value for each value needed to be used/called
+            setFirstOpenModel(false);
+            setFirstOpenPreP(true);
+            //Set Values
+            setMaker(data.maker);
+            setModel(data.model);
+            setVcode(data.vcode);
+            setStart(data.start);
+            setEnd(data.end);
+            setDpos(data.dripos);
+            setEcode(data.engcode);
+            setDisplace(data.disp);
+            setPtype(data.powered);
+            setFtype(data.fuel);
+            setTransc(data.transc);
+            setSpd(data.spd);
+            setTrans(data.trans);
+            setDt(data.dtrain);
         })
     }
-    //Fake Part Data
-    const data = [
-        {
-            PremiumCode: "DTX-209A",
-            ODmm: "190",
-            ODinch: "7.5",
-            IDmm: "132",
-            Major: "22.1",
-            Spline: "20"
-        },
-        {
-            PremiumCode: "DTX-209A",
-            ODmm: "190",
-            ODinch: "7.5",
-            IDmm: "132",
-            Major: "22.1",
-            Spline: "20"
-        },
-        {
-            PremiumCode: "DTX-209A",
-            ODmm: "190",
-            ODinch: "7.5",
-            IDmm: "132",
-            Major: "22.1",
-            Spline: "20"
-        },
-        {
-            PremiumCode: "DTX-209A",
-            ODmm: "190",
-            ODinch: "7.5",
-            IDmm: "132",
-            Major: "22.1",
-            Spline: "20"
-        },
-        {
-            PremiumCode: "DTX-209B",
-            ODmm: "200",
-            ODinch: "8.5",
-            IDmm: "132",
-            Major: "22.1",
-            Spline: "20"
-        },
-        {
-            SubPremiumCode: "DT-610U",
-            ODmm: "170",
-            ODinch: "7.5",
-            IDmm: "132",
-            Major: "22.1",
-            Spline: "20"
-        },
-        {
-            SubPremiumCode: "DT-611U",
-            ODmm: "175",
-            ODinch: "7.5",
-            IDmm: "132",
-            Major: "22.1",
-            Spline: "20"
-        },
-        {
-            SubPremiumCode: "DT-611U",
-            ODmm: "175",
-            ODinch: "7.5",
-            IDmm: "132",
-            Major: "22.1",
-            Spline: "20"
-        },
-        {
-            SubPremiumCode: "DT-611U",
-            ODmm: "175",
-            ODinch: "7.5",
-            IDmm: "132",
-            Major: "22.1",
-            Spline: "20"
-        },
-    ];
-    //Fake Competitor
-    const competitor = [
-        {
-            Name: "555",
-            Number: "555-xx"
-        },
-        {
-            Name: "ABC",
-            Number: "ABC-xx"
-        },
-        {
-            Name: "SYG",
-            Number: "SYG-xx"
-        }
-    ];
-    //Controlling Data Table
-    var preData = [{}], subPreData = [{}]
-    var cp=0, scp=0;
-    for (let i=0; i<data.length; i++) {
-        if (data[i].PremiumCode!==undefined) {
-            numOfPremium++;
-            preData[cp]=data[i];
-            cp++;
-        }
-        if (data[i].SubPremiumCode!==undefined) {
-            numOfSubPre++;
-            subPreData[scp]=data[i];
-            scp++;
-        }
+    console.log(maker, model, vcode, start, end, dpos, ecode, displace, ptype, ftype, transc, spd, trans, dt)
+        
+        //Querying Premium Parts Array
+    if (firstOpenPreP) { 
+        fetch("http://localhost:5000/exp/premium", {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+                "Acess-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, PATCH, OPTIONS"
+            },
+            body: JSON.stringify(carid)
+        })
+        .catch(err => {
+            return;
+        })
+        .then(res => {
+            if (!res || !res.ok || res.status>=400) {
+                return;
+            }
+            return res.json();
+        })
+        .then(data => {
+            if (!data) return;
+            setFirstOpenPreP(false);
+            setFirstOpenSPreP(true);
+            setPremiumData(data.Premium.TotalPre);
+        })
     }
+
+        //Querying SubPremium Parts Data
+    if (firstOpenSPreP) { 
+        fetch("http://localhost:5000/exp/subpremium", {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+                "Acess-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, PATCH, OPTIONS"
+            },
+            body: JSON.stringify(carid)
+        })
+        .catch(err => {
+            return;
+        })
+        .then(res => {
+            if (!res || !res.ok || res.status>=400) {
+                return;
+            }
+            return res.json();
+        })
+        .then(data => {
+            if (!data) return;
+            setFirstOpenSPreP(false);
+            setFirstOpenComp(true);
+            setSPremiumData(data.SPremium.TotalSPre);
+        })
+    }
+
+        //Querying Competitor
+    if (firstOpenComp) { 
+        fetch("http://localhost:5000/exp/comp", {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+                "Acess-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE, PATCH, OPTIONS"
+            },
+            body: JSON.stringify(carid)
+        })
+        .catch(err => {
+            return;
+        })
+        .then(res => {
+            if (!res || !res.ok || res.status>=400) {
+                return;
+            }
+            return res.json();
+        })
+        .then(data => {
+            if (!data) return;
+            setFirstOpenComp(false);
+            setCompetitor(data.Comp)
+        })
+    }
+
+    console.log(competitor)
+
     //Reducing First Header
     let namesArr = {};
-    var rowSpan = data.reduce((result, item, key) => {
+    var rowSpan = premiumData.reduce((result, item, key) => {
     if (namesArr[item.name] === undefined) {
         namesArr[item.name] = key;
         result[key] = 1;
@@ -147,7 +176,7 @@ function Specpdf() {
         const firstIndex = namesArr[item.name];
         if (
             firstIndex === key - 1 ||
-            (item.name === data[key - 1].name && result[key - 1] === 0)
+            (item.name === premiumData[key - 1].name && result[key - 1] === 0)
         ) {
             result[firstIndex]++;
             result[key] = 0;
@@ -158,12 +187,35 @@ function Specpdf() {
     }
     return result;
     }, []);
-    // console.log(rowSpan)
+
+    let snamesArr = {};
+    var srowSpan = spremiumData.reduce((result, item, key) => {
+    if (snamesArr[item.name] === undefined) {
+        snamesArr[item.name] = key;
+        result[key] = 1;
+    } else {
+        const firstIndex = snamesArr[item.name];
+        if (
+            firstIndex === key - 1 ||
+            (item.name === spremiumData[key - 1].name && result[key - 1] === 0)
+        ) {
+            result[firstIndex]++;
+            result[key] = 0;
+        } else {
+            result[key] = 1;
+            snamesArr[item.name] = key;
+        }
+    }
+    return result;
+    }, []);
+    
+    
     //Exports
     return (
         <div className="specpop">
             <div className="specheader">
-                <p className="specmodel">Totoya, Influx, KUN25 (2008-2011), RHD, 25DFTV, 2.5L, ICE, Diesel, -, SMT, FWD</p>
+                <p className="specmodel">{maker}, {model}, {vcode} {"("+start} - {end+")"}, {dpos}, {ecode}, {displace}, {ptype}, {ftype},
+                    {" "+transc}, {spd}{trans}, {dt}</p>
                 <button className='closingicon'>
                     X
                 </button>
@@ -201,7 +253,7 @@ function Specpdf() {
                                     <th rowSpan="2" colSpan="2" className="spectitle">
                                         AISIN Part
                                     </th>
-                                    <th colSpan="5" className="spectitle">
+                                    <th colSpan="10" className="spectitle">
                                         Dimension
                                     </th>
                                 </tr>
@@ -221,20 +273,60 @@ function Specpdf() {
                                     <th className="subspectitle">
                                         Spline
                                     </th>
+                                    <th className="subspectitle">
+                                        PCD (mm)
+                                    </th>
+                                    <th className="subspectitle">
+                                        Width OD<br />(mm)
+                                    </th>
+                                    <th className="subspectitle">
+                                        Length (inch)
+                                    </th>
+                                    <th className="subspectitle">
+                                        Length (mm)
+                                    </th>
+                                    <th className="subspectitle">
+                                        Height (mm)
+                                    </th>
                                 </tr>
                             </thead>
                             <tbody>
-                                {preData.map((val, key) => {
+                                {premiumData.map((val, key) => {
                                     return (
                                     <>
                                         <tr key={key}>
-                                            {rowSpan[key]>0 && <td rowSpan={numOfPremium} className="infoHeader">Premium</td>}
+                                            {rowSpan[key]>0 && <td rowSpan={premiumData.length} className="infoHeader">Premium</td>}
                                             <td className="speccode">{val.PremiumCode}</td>
                                             <td>{val.ODmm}</td>
                                             <td>{val.ODinch}</td>
                                             <td>{val.IDmm}</td>
                                             <td>{val.Major}</td>
                                             <td>{val.Spline}</td>
+                                            <td>{val.PCDmm}</td>
+                                            <td>{val.WidthOD}</td>
+                                            <td>{val.Lengthinch}</td>
+                                            <td>{val.Lengthmm}</td>
+                                            <td>{val.Heightmm}</td>
+                                        </tr>
+                                    </>
+                                    );
+                                })}
+                                {spremiumData.map((val, key) => {
+                                    return (
+                                    <>
+                                        <tr key={key}>
+                                            {srowSpan[key]>0 && <td rowSpan={spremiumData.length} className="infoHeader">Sub-Premium/AM</td>}
+                                            <td className="speccode">{val.SPremiumCode}</td>
+                                            <td>{val.ODmm}</td>
+                                            <td>{val.ODinch}</td>
+                                            <td>{val.IDmm}</td>
+                                            <td>{val.Major}</td>
+                                            <td>{val.Spline}</td>
+                                            <td>{val.PCDmm}</td>
+                                            <td>{val.WidthOD}</td>
+                                            <td>{val.Lengthinch}</td>
+                                            <td>{val.Lengthmm}</td>
+                                            <td>{val.Heightmm}</td>
                                         </tr>
                                     </>
                                     );
