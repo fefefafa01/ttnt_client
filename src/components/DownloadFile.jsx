bf05fa74f508c2ae7c498991a1f1314a9b784bb4
LeTranKhanh <khanh.lt@vn-cubesystem.com>
2023-07-18 11:14:26 +0700

    Bugfix: Header button skew changed, pre-install dependencies for AAT-6

--- src/components/DownloadFile.jsx
+++ src/components/DownloadFile.jsx
@@ -1,5 +1,6 @@
 import * as FileSaver from 'file-saver'
 import XLSX from 'sheetjs-style'
+import xls from 'xlsx'
 
 function DownloadFile (fileData, fileName, sheetName, fileExType) {
     /*  
@@ -30,35 +31,4 @@ function DownloadFile (fileData, fileName, sheetName, fileExType) {
     return (exportToExcel(fileData, fileName, sheetName, fileExType))
 }
 
-function Download1File2 (fileData1, fileData2, fileName, sheetName, fileExType) {
-    /*  
-    Imported Variable Guide:
-        - fileData = JSON Data imported from where function is called (preferably direct exported from Database Fetching)
-        - fileName = Your wished file name to export
-        - sheetName = *As the name suggested*
-        - fileExType = File Extension (xlsx for csv)
-    */
-
-    //File Configuration
-    const fileType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
-    const fileExtension = '.'+fileExType;
-
-    //Export in progress
-    const exportToExcel = async () => {
-        //New Worksheet and Workbook
-        const ws1 = XLSX.utils.json_to_sheet(fileData1);
-        const ws2 = XLSX.utils.json_to_sheet(fileData2);
-        let wb = XLSX.utils.book_new();
-        //Append Sheets into Workbook
-        XLSX.utils.book_append_sheet(wb, ws1, sheetName+" 1")
-        XLSX.utils.book_append_sheet(wb, ws2, sheetName+" 2")
-        //Buffering
-        const excelBuffer = XLSX.write(wb, { bookType: fileExType, type: 'array'});
-        const data = new Blob([excelBuffer], { type: fileType });
-        //Export
-        FileSaver.saveAs(data, fileName + fileExtension);
-    }
-    return (exportToExcel(fileData1, fileData2, fileName, sheetName, fileExType))
-}
-
-export {DownloadFile, Download1File2}
+export {DownloadFile}
